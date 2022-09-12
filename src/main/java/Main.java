f6f8f630cad7714e4374c65b14467c1acc1c942c
Tine-m <tinemarbjerg@gmail.com>
2022-09-12 10:10:40 +0200

    tilføjet get og set metoder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
     Person p1 = new Person(1.74);
-    Person p2 = new Person();
+    System.out.print(p1.getHøjde());
+    System.out.println();
+
+    p1.setHøjde(1.79);
+    System.out.print("ny højde " + p1.getHøjde());
+
   }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,12 +1,21 @@
 public class Person {
 
-  private double højdexxx;
+  private double højde;
+  private int alder;
 
   public Person(double højde) {
-    this.højdexxx = højde;
+    this.højde = højde;
   }
 
- public Person() {
+  public double getHøjde() {
+    return højde;
+  }
+
+  public void setHøjde(double højde){
+    this.højde = højde;
+  }
+
+  public void harFødselsdag() {
 
   }
 }
